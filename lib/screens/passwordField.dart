275d688d14fdd2d9a06cf43acf29ce0fdefeb9ff
satoshi <uchiyama.s@timeshappy.com>
2021-09-11 15:29:54 +0900

    login page making start.

--- lib/screens/passwordField.dart
+++ lib/screens/passwordField.dart
@@ -1,32 +1,30 @@
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class PassWordField extends StatefulWidget {
-  const PasswordField({
+  const PassWordField({
     this.fieldKey,
     this.hintText,
-    this.labelText,
     this.helperText,
     this.onSaved,
     this.validator,
     this.onFieldSubmitted,
-});
+    this.lavelText,
+  });
 
-  final Key? fieldKey;
-  final String? hintText;
-  final String? lavelText;
-  final String? helperText;
-  final FormFieldSetter<String>? onSaved;
-  final FormFieldvalidator<String>? validator;
-  final ValueChanged<String>? onFieldSubmitted;
+  final Key fieldKey;
+  final String hintText;
+  final String lavelText;
+  final String helperText;
+  final FormFieldSetter<String> onSaved;
+  final FormFieldValidator<String> validator;
+  final ValueChanged<String> onFieldSubmitted;
 
   @override
   _PassWordFieldState createState() => _PassWordFieldState();
-  }
+}
 
-class _PassWordFieldState extends State<PassWordField>{
+class _PassWordFieldState extends State<PassWordField> {
   bool _obscureText = true;
   @override
   Widget build(BuildContext context) {
@@ -37,8 +35,6 @@ class _PassWordFieldState extends State<PassWordField>{
       onSaved: widget.onSaved,
       validator: widget.validator,
       onFieldSubmitted: widget.onFieldSubmitted,
-
-    )
+    );
   }
 }
-
